e85cb7909f9238d531201cccd5b14710aaca15bd
Philippe Lanougadere <soufiane92230@gmail.com>
2020-11-25 22:57:36 +0100

    Create HTML layout

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const svgColor= [
+    '#9aa0a6',
+    '#4285f4',
+    '#34a853',
+    '#f4b400',
+    '#ea4335',
+    ]
+    
+    let count = 0;
+    document.querySelectorAll('path').forEach(item =>{
+        item.style.fill = svgColor[count]; 
+        count++;    
+    })
+    
+    let microPopup = document.createElement('div');
+    microPopup.classList.add('popup'); 
+    microPopup.innerText ='Search by voice';
+    
+    
+    document.querySelector('svg:nth-child(2)').addEventListener('mouseover', ()=>{
+        microPopup.style.opacity=1;
+    
+        document.querySelector('.search-bar').append(microPopup);
+    })
+    
+    document.querySelector('svg:nth-child(2)').addEventListener('mouseout', ()=>{
+        microPopup.style.opacity=0
+    })
+    
+    
