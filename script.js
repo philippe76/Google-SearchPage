0489b2e8cf32e83845033b277bd9b28da0469889
Philippe Lanougadere <soufiane92230@gmail.com>
2020-11-27 15:30:16 +0100

    Improve style first search results area

--- script.js
+++ script.js
@@ -1,36 +1,56 @@
 
+// FILL SVG WITH COLORS
 const svgColor= [
     '#80868b',
     '#4285f4',
     '#34a853',
     '#f4b400',
     '#ea4335',
-    '#4285f4'
-    // '#9aa0a6',
+    '#4285f4',
+    '#5f6368',
+    '#34a853',
+    '#ea4335',
+    '#fbbc04',
+    '#4285f4',
+    '#5f6368',
+    '#5f6368',
+    '#5f6368',
+    '#5f6368',
+    '#5f6368',
+]
+    
+let count = 0;
+document.querySelectorAll('path').forEach(item => {
+    item.style.fill = svgColor[count]; 
+    count++;    
+})
 
 
- 
-    
-    ]
-    
-    let count = 0;
-    document.querySelectorAll('path').forEach(item =>{
-        item.style.fill = svgColor[count]; 
-        count++;    
+// ANIMATE POP-UPS
+let microPopup = document.createElement('div');
+microPopup.classList.add('popup'); 
+microPopup.innerText ='Search by voice';
+
+document.querySelector('svg:nth-child(2)').addEventListener('mouseover', ()=> {
+    microPopup.style.opacity=1;
+    document.querySelector('.search-bar').append(microPopup);
+})
+
+document.querySelector('svg:nth-child(2)').addEventListener('mouseout', ()=> {
+    microPopup.style.opacity=0
+})
+
+
+// GIVE OR REMOVE ACTIVE CLASS
+document.querySelectorAll('.search-choice > ul > li').forEach( item => {
+    item.addEventListener('mouseover', (e)=> {
+       item.classList.add('choice-active')
     })
-    
-    let microPopup = document.createElement('div');
-    microPopup.classList.add('popup'); 
-    microPopup.innerText ='Search by voice';
-    
-    
-    document.querySelector('svg:nth-child(2)').addEventListener('mouseover', ()=>{
-        microPopup.style.opacity=1;
-    
-        document.querySelector('.search-bar').append(microPopup);
+})
+
+document.querySelectorAll('.search-choice > ul > li').forEach( item => {
+    item.addEventListener('mouseout', (e)=> {
+       item.classList.remove('choice-active')
     })
-    
-    document.querySelector('svg:nth-child(2)').addEventListener('mouseout', ()=>{
-        microPopup.style.opacity=0
-    })
+})
 
