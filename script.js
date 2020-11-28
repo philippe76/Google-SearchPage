273d3c674ca6dd437397255a42fb94784fd51eb9
Philippe Lanougadere <soufiane92230@gmail.com>
2020-11-28 19:38:37 +0100

    Finish moving HTML content to JS objects

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ window.addEventListener("scroll", () => {
 })
 
 
-// FILL RESULT AREAS
+// FILL RESULT AREAS HTML
 let result_subtitle = [...document.querySelectorAll('.result h5')];
 let result_link = [...document.querySelectorAll('.result a')];
 let result_title = [...document.querySelectorAll('.result h3')];
@@ -91,21 +91,54 @@ let result_content = [
         title: 'build this webpage - GitHub Pages',
         para: 'With zero coding experience, artist <strong>building</strong> 180 webpages . ... days ago, having never done any programming in her life, Jennifer Dewalt built her first <strong>webpage</strong>.'
     },
+    {
+        sub: 'www.websitebuilderexpert.com › building-websites',
+        href: 'https://www.websitebuilderexpert.com/building-websites/',
+        title: 'How to Build a Website 2020 | The Complete Beginner\'s Guide',
+        para: '4 days ago — How to <strong>Build</strong> a <strong>Website</strong> from Scratch. We\'ll look at how to make a <strong>website</strong> with both a <strong>website</strong> builder and WordPress, with no steps left out.'
+    },
+    {
+        sub: 'websitesetup.org',
+        href: 'https://websitesetup.org/',
+        title: 'How to Create a Website: Step-by-Step Guide for Beginners ...',
+        para: 'Learn how to make a <strong>website</strong> and set it up. This site contains different tutorials on how to <strong>build</strong> a <strong>website</strong> using WordPress, Joomla, Drupal & HTML.'
+    },
+    {
+        sub: 'www.google.com › business › website-builder',
+        href: 'https://www.google.com/intl/en/business/website-builder/',
+        title: 'Free Website Builder For Your Business - Google My Business',
+        para: 'Let Google <strong>build</strong> your first <strong>website</strong> for free. We\'ll help you create a beautiful <strong>website</strong> to impress and engage new customers – and establish your business online.'
+    },
+    {
+        sub: 'www.webnode.com',
+        href: 'https://www.webnode.co.uk/',
+        title: 'Webnode: Create a free website easily | Free website builder',
+        para: 'Easily <strong>build</strong> your <strong>website</strong> with Webnode online editor. Domain names and hosting are included. Fast and local support will take care of you..'
+    },
+    {
+        sub: 'www.webhostingsecretrevealed.net › WHSR Blog',
+        href: 'https://www.webhostingsecretrevealed.net/blog/web-hosting-guides/make-a-website/',
+        title: 'How to Create a Website: Step-by-Step Tutorial',
+        para: '<strong>Creating</strong> from scratch; Using a content management system (CMS); Using a <strong>website</strong> builder. You get more flexibility in ...'
+    },
+    {
+        sub: 'www.squarespace.com',
+        href: 'https://www.squarespace.com/',
+        title: 'Squarespace: Build a Website – Website Builder',
+        para: 'Squarespace is the all-in-one solution for anyone looking to create a beautiful <strong>website</strong>. Domains, eCommerce, hosting, galleries, analytics, and 24/7 support all ....'
+    },
+    {
+        sub: '<strong>Ad·</strong> www.wix.com/build-website-for-free',
+        href: 'https://www.wix.com/html5en/hiker-build?gclid=Cj0KCQiAh4j-BRCsARIsAGeV12C2byf88U10YGyvAIeCYzPxs65QC2pDIqQVz-d8jF6O2ux7rQ77jBoaAlRwEALw_wcB&utm_medium=cpc&experiment_id=build+webpages%5Ee%5E48579939340%5E',
+        title: 'Build a Webpage for Free',
+        para: 'Make Professional <strong>Webpages</strong> for Free Fast & Easy Free <strong>Website</strong> Builder! Market leaders. Multiple payment methods. Social media compatible. SEO wizard. 100s of Apps. Mobile optimized. Free multilingual fonts. Easy drag n’ drop. Easy-to-add blog. 100s of templates. ‎Online Store Builder · ‎Free Website Builder · ‎Free Hosting · ‎Customize Easily'
+    },
 ]
 
-for (let i=0; i<3; i++) {
+for (let i=0; i<result_content.length; i++) {
     result_subtitle[i].innerHTML = `${result_content[i].sub} <i class="fas fa-caret-down"></i>`;
-}
-
-for (let i=0; i<3; i++) {
-    result_link[i].setAttribute('href', result_content[i].href)
-}
-
-for (let i=0; i<3; i++) {
-    result_title[i].innerHTML += result_content[i].title
-}
-
-for (let i=0; i<3; i++) {
-    result_para[i].innerHTML = result_content[i].para
+    result_link[i].setAttribute('href', result_content[i].href);
+    result_title[i].innerHTML += result_content[i].title;
+    result_para[i].innerHTML = result_content[i].para;
 }
 
